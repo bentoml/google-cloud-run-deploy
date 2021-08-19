db2363c14f0773120abeb90b1b78dfc7ed0b344d
David Ascencios-Rondon <david.ascencios@pucp.pe>
2021-08-19 02:55:01 -0500

    Update describe.py

--- describe.py
+++ describe.py
@@ -5,17 +5,20 @@ from ruamel.yaml import YAML
 from utils import generate_cloud_run_names, run_shell_command
 
 
-def describe_cloud_run(deployment_name, return_json=False):
+def describe_cloud_run(deployment_name,  cloud_run_config, return_json=False):
     service_name, _ = generate_cloud_run_names(deployment_name)
 
     if not return_json:
         stdout, stderr = run_shell_command(
-            ["gcloud", "run", "services", "describe", service_name]
+            ["gcloud", "run", "services", "describe", service_name,
+            "--platform", str(cloud_run_config["platform"])]
         )
         print(stdout)
     else:
         stdout, stderr = run_shell_command(
-            ["gcloud", "run", "services", "describe", service_name, "--format=export"]
+            ["gcloud", "run", "services", "describe", service_name, 
+             "--platform", str(cloud_run_config["platform"]),
+             "--format=export"]
         )
         yaml = YAML()
         data = yaml.load(stdout)
